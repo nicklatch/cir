8ea5b4702a664f471392ccdd6c1a5847a2e34886
Nick Latcham <nick.latcham@proton.me>
2025-04-04 14:51:00 -0500

    feat: update nav items

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,24 +1,50 @@
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
+import { Separator } from './ui/separator';
 
-export function NavMain({ items = [] }: { items: NavItem[] }) {
+interface NavMainProps {
+    quickItems: Array<NavItem>
+    raceDayItems: Array<NavItem>
+}
+
+export function NavMain({ quickItems = [], raceDayItems = [] }: NavMainProps) {
     const page = usePage();
     return (
-        <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Platform</SidebarGroupLabel>
-            <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild isActive={item.href === page.url}>
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-        </SidebarGroup>
+        <>
+            <SidebarGroup className="px-2 py-0">
+                <SidebarGroupLabel>Quick Views</SidebarGroupLabel>
+                <SidebarMenu>
+                    {quickItems.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton asChild isActive={item.href === page.url}>
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroup>
+            <Separator />
+            <SidebarGroup className="px-2 py-0">
+                <SidebarGroupLabel>
+                    Race Day
+                </SidebarGroupLabel>
+                <SidebarMenu>
+                    {raceDayItems.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton asChild isActive={item.href === page.url}>
+                                <Link href={item.href} prefetch>
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroup>
+        </>
     );
 }

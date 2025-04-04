606b310fcb39168eb129dff0fbe4af5b9ec95d42
Nick Latcham <nick.latcham@proton.me>
2025-04-04 16:21:42 -0500

    test: add CreateDriverTest

---
+++ tests/Feature/Driver/DriverCreateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Driver;
+use App\Models\User;
+
+uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
+
+test('driver index page is displayed', function () {
+    $user = User::factory()->create();
+
+    $this
+        ->actingAs($user)
+        ->get('/drivers')
+        ->assertOk();
+});
+
+test('driver can be created', function () {
+    $user = User::factory()->create();
+
+    $driver = [
+        'first_name' => 'Test',
+        'last_name' => 'Driver',
+        'phone_number' => '5558675309',
+        'car_number' => '99',
+        'drive_type' => 'FWD',
+    ];
+
+    $response = $this
+        ->actingAs($user)
+        ->post('/drivers/create', $driver);
+
+    $response
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/drivers');
+
+    $created_driver = Driver::where('first_name', 'Test')->get()->first();
+
+    expect($created_driver->first_name)->toBe('Test');
+    expect($created_driver->last_name)->toBe('Driver');
+    expect($created_driver->phone_number)->toBe('5558675309');
+    expect($created_driver->car_number)->toBe('99');
+    expect($created_driver->drive_type)->toBe('FWD');
+});

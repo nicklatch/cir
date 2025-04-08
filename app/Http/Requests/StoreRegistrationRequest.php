6d950a5d3da9f714afe7120fef975be10d48590b
Nick Latcham <nick.latcham@proton.me>
2025-04-08 13:16:28 -0500

    chore: spelling

--- app/Http/Requests/StoreRegistrationRequest.php
+++ app/Http/Requests/StoreRegistrationRequest.php
@@ -19,7 +19,7 @@ class StoreRegistrationRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to tte request.
+     * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */

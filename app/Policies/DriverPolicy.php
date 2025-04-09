3dbcc9a0bba6738c88b0d02415c91902e66ba77d
Nick Latcham <nick.latcham@proton.me>
2025-04-08 19:54:39 -0500

    feat: create DriverPolicy

---
+++ app/Policies/DriverPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class DriverPolicy
+{
+    use HandlesAuthorization;
+
+    public function store(User $user): bool
+    {
+        return $user->can('create drivers');
+    }
+
+    public function index(User $user): bool
+    {
+        return $user->can('view drivers');
+    }
+
+    public function update(User $user): bool
+    {
+        return $user->can('update drivers');
+    }
+
+    public function destroy(User $user): bool
+    {
+        return $user->can('delete drivers');
+    }
+}
